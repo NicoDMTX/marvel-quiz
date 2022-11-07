6e3d625dfd60cfb9f9cc777e3c6c1bac97c98664
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-11-07 16:54:15 +0100

    update firebaseconfig

---
+++ src/components/Firebase/firebaseConfig.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyDnj7kI3WZwPujwGJJ8YGRYB9sR6BRgKoc",
+    authDomain: "marvel-quiz-3a881.firebaseapp.com",
+    projectId: "marvel-quiz-3a881",
+    storageBucket: "marvel-quiz-3a881.appspot.com",
+    messagingSenderId: "507895446984",
+    appId: "1:507895446984:web:cdbd242e517817e41d14fd"
+  };
+
+initializeApp(config)
+
+export default Firebase;
