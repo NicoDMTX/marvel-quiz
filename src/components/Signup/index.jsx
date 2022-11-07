278e937484b4d40f6bb7bee49a8f22aea19cba6d
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-11-07 17:46:52 +0100

    Update signup index

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+  const data = {
+    pseudo: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  const [loginData, setLoginData] = useState(data)
+
+  const handleChange = e => {
+    setLoginData({...loginData, [e.target.id]: e.target.value})
+  }
+
+  const { pseudo, email, password, confirmPassword } = loginData;
+
+  const btn = pseudo === '' || email === '' || password === '' || password !== confirmPassword ?
+  <button disabled>Inscription</button> : <button>Inscription</button>
+
   return (
     <div className='signUpLoginBox'>
         <div className='slContainer'>
-            Signup
+            <div className='formBoxLeftSignup'>
+            </div>
+            <div className='formBoxRight'>
+              <div className="formContent">
+                <form>
+                  <h2>Inscription</h2>
+                  <div className='inputBox'>
+                    <input onChange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required />
+                    <label htmlFor='pseudo'>Pseudo</label>
+                  </div>
+
+                  <div className='inputBox'>
+                    <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required />
+                    <label htmlFor='email'>Email</label>
+                  </div>
+
+                  <div className='inputBox'>
+                    <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required />
+                    <label htmlFor='password'>Mot de passe</label>
+                  </div>
+
+                  <div className='inputBox'>
+                    <input onChange={handleChange} value={confirmPassword} type="password" id="confirmPassword" autoComplete="off" required />
+                    <label htmlFor='confirmPassword'>Confirmez le mot de passe</label>
+                  </div>
+
+                  {btn}
+                </form>
+              </div>
+            </div>
         </div>    
     </div>
   )
